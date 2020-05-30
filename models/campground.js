c5a0eaffba41264e2479ca531ff447b342d8e3f6
Everardo Ortiz <everardo.ortiz.5@gmail.com>
2020-05-30 11:46:08 +0800

    add mongoose atlas to models/

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://evortiz55:Cathay74@cluster0-vbnlb.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false
+}).then(() => {
+    console.log('Connected to DB');
+}).catch(err => {
+    console.log('ERROR:', err.message);
+});
 
 // Schema Setup
 var campgroundSchema = new mongoose.Schema({
